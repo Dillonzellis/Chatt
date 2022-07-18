39557a63b4a6004ee3b49e673ee387913a59bb02
Dillon Ellis <dellis@fcul.com>
2022-07-18 10:53:12 -0400

    original covid resources page

---
+++ covid-resources.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+<title>Chattahoochee Federal Credit Union - Additional COVID-19 Resources</title>
+
+<?php include('includes/head.php');?>
+
+
+</head>
+
+<body>
+<header id="header" class="container u-full-width">
+
+<?php include('includes/header.php');?>
+
+    <div class="page-title">Additional COVID-19 Resources</div>
+
+<div id="logo"><a href="https://chatt.coop/index.php"><img src="https://chatt.coop/images/logo/logo-with-background.png" alt="Chattahoochee FCU Logo"/></a></div><!-- end logo-->
+
+</header>
+
+
+<main id="main" class="content page">
+
+<div class="container row">
+	<P><strong>SCAM ALERTS:</strong> Scammers are already  taking advantage of an unfortunate situation and soliciting funds from  unknowing targets. Make sure you are aware of the latest COVID-19 related scams  and do not fall victim.</>
+	<UL>
+		<LI><a href="#" title="Criminals Call or Email Consumers  Pretending to be from WHO" data-remodal-target="cyber-security" target="_blank"><strong>Criminals Call or Email Consumers  Pretending to be from WHO</strong></a></LI>
+		<LI><a href="#" title="Phishing Emails,  Social Media Ads &amp; Fake Amazon Products" data-remodal-target="yahoo-covid" target="_blank"><strong>Phishing Emails,  Social Media Ads &amp; Fake Amazon Products</strong></a></LI>
+		<LI><a href="#" title="FBI Sees Rise in  Fraud Schemes Related to the Coronavirus (COVID-19) Pandemic" data-remodal-target="lscu-covid" target="_blank"><strong>FBI Sees Rise in  Fraud Schemes Related to the Coronavirus (COVID-19) Pandemic</strong></a></LI></UL>
+	<P><strong><u>CDC has resources on COVID-10.</u></strong>  Access and post these <a href="#" title="free printed  resources" data-remodal-target="cdc-covid" target="_blank"><strong>free printed  resources</strong></a> from  the CDC to understand the coronavirus (COVID-19) facts and help protect your  staff and members!</P>
+	<UL><LI> <a href="#" title="Coronavirus Fact  Sheet" data-remodal-target="cdc-factsheet" target="_blank"><strong>Coronavirus Fact  Sheet</strong></a></LI>
+		<LI><a href="#" title="What to do if  you're sick" data-remodal-target="sick" target="_blank"><strong>What to do if  you&rsquo;re sick</strong></a></LI>
+		<LI><a href="#" title="Stop the spread of  germs" data-remodal-target="cdc-spread" target="_blank"><strong>Stop the spread of  germs</strong></a></LI>
+		<LI> <a href="#" title="Symptoms of  coronavirus" data-remodal-target="symptoms" target="_blank"><strong>Symptoms of  coronavirus</strong></a></LI></UL>
+<P><strong><u>Credit Union National Association (CUNA)</u></strong> released five steps consumers should  take to exercise financial caution in the face of a global pandemic:</p>
+<ol>
+  <LI><strong>Contact       Your Financial Institution </strong><br>
+    Credit unions from coast to coast are adjusting their operations to help       stem the spread of coronavirus and protect members and employees. Make       sure you're up to date on the latest information on lobby closures, hours       of operation, and drive-thru services and ATM access by contacting or       visiting the website of your financial institution.&nbsp;</LI>
+  <LI><strong>Protect       Your Data</strong><br>
+    Be vigilant about protecting your digital identity and personal data. If       your credit union reaches out, they won't ask for confidential information       such as your name, password, personal identification number (PIN) or other       account information. A  <a href="#" title="recent report from NBC News" data-remodal-target="covid-report" target="_blank">recent report       from NBC News</a> notes that criminals are using COVID-19 as a basis       for email attacks to snag personal information, steal money and infect       computers with malware.</li>
+  <li><strong>Utilize       Online and Mobile Services</strong><br>
+    You can easily deposit a check using your mobile device or quickly and       securely send money to others with digital payments. Using credit or debit       cards for payments is especially important now when we all are being asked       to practice social distancing. You can still meet most of your obligations       without leaving your home, thanks to your credit union.&nbsp;</li>
+  <li><strong>Research       Local Financial Support</strong><br>
+    From Miami to Seattle, credit unions are extending low- and no-interest       loans, offering fee-free loan deferrals, and finding other individualized       solutions that help ensure those facing financial disruptions due to       COVID-19 can keep the lights on, keep food on the table, and maintain a       secure financial future once the country makes it through this public       health emergency.&nbsp;</li>
+  <li><strong>Remember:       Your Money is Safest in a Credit Union</strong><br>
+    There are a lot of things to worry about these days, but the safety of       your money in your credit union account aren't one of them. Your money is       safer in your insured credit union than it is in your pockets or under       your mattress. There is no risk to keeping money in your credit union       account, but there are countless risks to holding cash, <a href="#" title="as pointed out       in this Wall Street Journal piece" data-remodal-target="covid-wallstreet" target="_blank">as pointed out       in this Wall Street Journal piece</a>.&nbsp;&nbsp;&nbsp;&nbsp;    </li>
+</ol>
+</div><!--end container row-->
+
+</main>
+
+
+<footer id="footer" class="row container-padding">
+
+<?php include('includes/footer.php');?>
+
+</footer>
+
+<?php include('includes/js-includes.php');?>
+
+</body>
+
+</html>
